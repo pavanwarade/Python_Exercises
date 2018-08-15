13a39161b42ac060b0a61b41fbbca264b91dec19
PAVAN WARADE <pavanswarade@gmail.com>
2018-08-15 21:44:16 +0530

    Update Matplotlib.py

--- Exercises/Matplotlib.py
+++ Exercises/Matplotlib.py
@@ -1,24 +1,24 @@
 
+# Data Visualisation using Python
+
 import matplotlib.pyplot as plt
+
+#Data given in tuple format
 x=(2,5,4,8,9,6,3,10)
+
+#giving color to the graph
 plt.plot(x,"-ro")
 plt.ylabel("red")
 plt.xlabel("yellow")
 plt.show()
+
+
+
+
 """
-
-
 import Module
 
 obj1=Module.class1()
 obj2=Module.class2()
 
 """
-
-
-
-
-
-
-
-
