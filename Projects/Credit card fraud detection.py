b1ccefdb86443a6996a666f2e80a688398ad72cf
PAVAN WARADE <pavanswarade@gmail.com>
2018-08-15 22:02:58 +0530

    Add files via upload

---
+++ Projects/Credit card fraud detection.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+print('Python: {}'.format(sys.version))
+print('Numpy: {}'.format(numpy.__version__))
+print('Pandas: {}'.format(pandas.__version__))
+print('Matplotlib: {}'.format(matplotlib.__version__))
